381e7c7dcfbf770123dffbddf18686fda94bda3e
Allie Grampa <agrampa@yahoo.com>
2017-07-25 22:06:02 -0700

    added hash function

--- hash-tables/dll-hash.js
+++ hash-tables/dll-hash.js
@@ -9,3 +9,11 @@ const DLLHashTable = module.exports = function(sixe=8192) {
     this.buckets[i] = new DLL():
   }
 };
+
+// O(1)
+DLLHashTable.prototype.hashKey = function(key) {
+  if(!key) throw new Error('Key required');
+
+  let hash = kep.split('').reduce((acc, current) => acc + current.charCodeAt(0), 0) % this.size;
+  return hash;
+};
