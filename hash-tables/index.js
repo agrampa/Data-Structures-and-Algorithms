4368cdd102e47c7bef89489c925e263f121ebf34
Allie Grampa <agrampa@yahoo.com>
2017-07-26 08:13:43 -0700

    added eslintrc, gitignore, index.js for examples

---
+++ hash-tables/index.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const DLL = require('./lib/dll.js');
+let dll = new DLL();
+dll.append(1);
+// console.log('dll with one node, val 1: ', dll);
+dll.append(2);
+// console.log('dll with a second node appended, val 2: ', dll);
+// dll.append(3);
+// console.log('dll with a third node appended, val 3: ', dll);
+// console.log('head.prev', dll.head);
+// console.log('dll with a third node appended, val 3: ', dll);
+// dll.prepend(4);
+// console.log('dll with a node prepended, val 4: ', dll);
+// dll.prepend(5);
+// console.log('dll with a second node prepended, val 5: ', dll);
+// dll.prepend(6);
+// console.log('dll with a third node prepended, val 6: ', dll);
+// console.log('tail', dll.tail);
+
+//
+// let dllRemove = new DLL();
+// dllRemove.append(6);
+// // console.log('after append 6', dllRemove);
+// dllRemove.append(5);
+// // console.log('after append 5', dllRemove);
+// dllRemove.append(4);
+// // console.log('after append 4', dllRemove);
+// dllRemove.append(3);
+// // console.log('after append 3', dllRemove);
+// dllRemove.remove(4);
+// console.log('after remove 4', dllRemove);
+// console.log('after append 3', dllRemove.tail.next.next);
+// dllRemove.append(2);
+// console.log(dllRemove);
+// dllRemove.append(1);
+// console.log(dllRemove);
+// dllRemove.remove(3);
+// console.log('before remove', dllRemove.tail.next.next);
+// // console.log(dllRemove);
+// console.log('after remove', dllRemove.tail.next.next);
+
+const DLLHashTable = require('./lib/dll-hash.js');
+let hash = new DLLHashTable();
+// console.log('hash', hash);
+hash.set('test key', 'test value');
+hash.set('test2', 'value2');
+hash.set('test3', 'value3');
+console.log('hash', hash);
+// console.log(hash.buckets[1]);
+// hash.get('test key');
+console.log('hash get', hash.get('test3'));
+console.log('hash get', hash.get('blah'));
+hash.remove('test key');
+console.log('hash', hash);
