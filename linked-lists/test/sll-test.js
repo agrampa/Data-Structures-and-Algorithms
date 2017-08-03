b5730487b711aab140f7fcdb4a20f2ee59a40f41
Allie Grampa <agrampa@yahoo.com>
2017-08-03 12:53:59 -0700

    partway through tests

---
+++ linked-lists/test/sll-test.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const expect = require('chai').expect;
+
+const Node = require('../node.js');
+const SLL = require('../sll.js');
+
+let sll = new SLL();
+
+describe('Testing the single linked-list methods', function() {
+  describe('Instantiating a new SLL', done => {
+    it('should instantiate a new linked list as an object', done => {
+      expect(sll).to.be.a('object');
+      done();
+    });
+
+    it('should not be null', done => {
+      expect(sll).to.exist;
+      done();
+    });
+
+    it('should not be empty', done => {
+      expect(sll).to.not.be.empty;
+      done();
+    });
+  });
+
+  describe('Testing the append method', function() {
+    it('should not contain the value before appending', done => {
+      expect(sll).to.not.have.valueOf(2);
+      done();
+    });
+
+    it('should contain the appended value', done => {
+      sll.append(2);
+      expect(sll).to.have.valueOf(2);
+      done();
+    });
+
+    it('should add a new node at the end of the linked list', done => {
+      expect(sll.head.next).to.equal.valueOf(2);
+      done();
+    });
+
+    it('should add a third node to the linked list', done => {
+      sll.append(3);
+      expect(sll).to.have.valueOf(3);
+      done();
+    });
+
+    it('should add the node at the end of the linked list', done => {
+      expect(sll.head.next.next).to.equal.valueOf(3);
+      done();
+    });
+  });
+
+
+  describe('Testing the prepend method', function() {
+
+  });
+});
