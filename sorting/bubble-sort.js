e035bf354d67faef06d3c3906f2242bfa99cf977
Allie Grampa <agrampa@yahoo.com>
2017-08-15 11:35:36 -0700

    updated bubble sort

--- sorting/bubble-sort.js
+++ sorting/bubble-sort.js
@@ -13,14 +13,15 @@
 // O(n^2)
 const bubbleSort = nums => {
   do {
-    let swapped = false;
+    let swapped = false
     for(let i = 0; i < nums.length; i++) {
       if (nums[i] > nums[i+1]) {
-        const temp = nums[i];
-        nums[i] = nums[i+1];
-        nums[i+1] = temp;
-        swapped = true;
+        const temp = nums[i]
+        nums[i] = nums[i+1]
+        nums[i+1] = temp
+        swapped = true
       }
     }
-  } while (swapped);
+  } while (swapped)
+  return nums
 }
