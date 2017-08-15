414a3b36293f666c86704680084e5bea3dfcbcba
Allie Grampa <agrampa@yahoo.com>
2017-08-15 12:44:04 -0700

    added merge sort demo with console logs from repl

--- sorting/merge-sort.js
+++ sorting/merge-sort.js
@@ -41,3 +41,49 @@ const merge = (left, right) => {
 
   return results
 }
+
+
+// with some console.log messages thrown in for readability in repl
+
+const mergeSort = nums => {
+  if(nums.length < 2) return nums
+
+  const length = nums.length
+  const middle = Math.floor(length / 2)
+  const left = nums.slice(0, middle)
+  const right = nums.slice(middle, length)
+
+  return merge(mergeSort(left), mergeSort(right))
+}
+
+const merge = (left, right) => {
+  const results = []
+  console.log('left', left)
+  console.log('right', right)
+
+  while (left.length && right.length) {
+    console.log('while left and right')
+    if(left[0] <= right[0]) {
+      results.push(left.shift())
+    } else {
+      results.push(right.shift())
+    }
+    console.log('results after while left and right', results)
+  }
+
+  while(left.length) {
+    console.log('while left')
+    results.push(left.shift())
+    console.log('results after while left', results)
+  }
+
+  while(right.length) {
+    console.log('while right')
+    results.push(right.shift())
+    console.log('results after whie right', results)
+  }
+  console.log('results', results)
+  return results
+}
+
+mergeSort([8, 3, 5, 1, 9, 12, 4, 2, 18])
