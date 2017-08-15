be5c0dce4cfbd9238f0993644fcbbd35af887b2c
Allie Grampa <agrampa@yahoo.com>
2017-08-15 12:23:06 -0700

    updated quick sort with example output from repl

--- sorting/quick-sort.js
+++ sorting/quick-sort.js
@@ -23,3 +23,40 @@ const quickSort = nums => {
 
   return [...quickSort(left), pivot, ...quickSort(right)]
 }
+
+
+// from repl:
+// console.log(nums[i])
+// console.log(left)
+// console.log(right)
+
+quickSort([4, 7, 2, 8, 9, 6])
+
+4
+left [ 4 ]
+right []
+7
+left [ 4 ]
+right [ 7 ]
+2
+left [ 4, 2 ]
+right [ 7 ]
+8
+left [ 4, 2 ]
+right [ 7, 8 ]
+9
+left [ 4, 2 ]
+right [ 7, 8, 9 ]
+4
+left []
+right [ 4 ]
+7
+left [ 7 ]
+right []
+8
+left [ 7, 8 ]
+right []
+7
+left [ 7 ]
+right []
+=> [ 2, 4, 6, 7, 8, 9 ]
