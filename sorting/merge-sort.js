0095fa28a6579a15afcfdebf5c19af79550303a3
Allie Grampa <agrampa@yahoo.com>
2017-08-15 17:51:34 -0700

    added another example of merge sort solution

--- sorting/merge-sort.js
+++ sorting/merge-sort.js
@@ -87,3 +87,45 @@ const merge = (left, right) => {
 }
 
 mergeSort([8, 3, 5, 1, 9, 12, 4, 2, 18])
+
+
+
+// From lecture demo code:
+
+// takes in two sorted lists
+// returns a sorted list
+const merge = (left, right){
+  let result = []
+
+  while(left.length || right.length){
+    if(!left.length){
+      result.concat(right)
+      return result
+    }
+
+    if(!right.length){
+      result.concat(left)
+      return result
+    }
+
+    if(left[0]< right[0]){
+      result.push(left.shift())
+    } else {
+      result.push(right.shift())
+    }
+  }
+
+  return result
+}
+
+let mergeSort = module.exports = (items) => {
+  // allready sorted
+  if(items.length < 2)
+    return items
+
+  let middleIndex = Math.floor(items.length / 2)
+  let left = items.slice(0, middleIndex)
+  let right = items.slice(middleIndex)
+
+  return merge(mergeSort(left), mergeSort(right))
+}
